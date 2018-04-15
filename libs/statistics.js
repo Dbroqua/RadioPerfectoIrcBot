350ddede76378d6ce46ad5d364c1bdd6c650742e
dbroqua <dbroqua@mousur.org>
2018-04-15 19:42:10 +0200

    Fixed bug (again)

--- libs/statistics.js
+++ libs/statistics.js
@@ -73,39 +73,39 @@ class Statistics {
             };
 
             that.db.find('histories', query, function(err, res) {
-                    if (err) {
-                        callback('#500 - Impossible de te répondre pour le moment !', null);
-                    } else {
-                        let _statistics = [],
-                            _max = 16,
-                            _length = res.length;
+                if (err) {
+                    callback('#500 - Impossible de te répondre pour le moment !', null);
+                } else {
+                    let _statistics = [],
+                        _max = 16,
+                        _length = res.length;
 
-                        for (let i = 0; i < _length; i++) {
-                            let _found = false;
-                            for (let j = 0; j < _statistics.length; j++) {
-                                if (_statistics[j].artist === res[i].artist) {
-                                    _statistics[j].played++;
-                                    _found = true;
-                                    break;
-                                }
-                            }
-                            if (_found === false) {
-                                _statistics.push({
-                                    artist: res[i].artist,
-                                    songName: res[i].songName,
-                                    played: 1
-                                });
+                    for (let i = 0; i < _length; i++) {
+                        let _found = false;
+                        for (let j = 0; j < _statistics.length; j++) {
+                            if (_statistics[j].artist === res[i].artist) {
+                                _statistics[j].played++;
+                                _found = true;
+                                break;
                             }
                         }
+                        if (_found === false) {
+                            _statistics.push({
+                                artist: res[i].artist,
+                                songName: res[i].songName,
+                                played: 1
+                            });
+                        }
+                    }
 
-                        _statistics.sort(function(a, b) {
-                            return a.played < b.played ? 1 : -1;
-                        });
+                    _statistics.sort(function(a, b) {
+                        return a.played < b.played ? 1 : -1;
+                    });
 
-                        for (let i = 0; i < _statistics.length; i++) {
-                            _statistics[i].text = _statistics[i].artist + ' a été entendu ' + _statistics[i].played + ' fois';
+                    for (let i = 0; i < _statistics.length; i++) {
+                        _statistics[i].text = _statistics[i].artist + ' a été entendu ' + _statistics[i].played + ' fois';
 
-                            if (i > _max)) {
+                        if (i > _max) {
                             break;
                         }
                     }
@@ -113,69 +113,69 @@ class Statistics {
                     callback(null, _statistics);
                 }
             });
+        }
     }
-}
 
-lastplayed(from, search, callback) {
-    let that = this,
-        query = {
-            find: {
-                $or: [{
-                        artist: formatString(search)
-                    },
-                    {
-                        songName: formatString(search)
-                    }
-                ]
-            },
+    lastplayed(from, search, callback) {
+        let that = this,
+            query = {
+                find: {
+                    $or: [{
+                            artist: formatString(search)
+                        },
+                        {
+                            songName: formatString(search)
+                        }
+                    ]
+                },
+                sort: {
+                    createdAt: 'desc'
+                },
+                limit: 1
+            };
+
+        that.db.find('histories', query, function(err, res) {
+            if (err) {
+                callback('#500 - Impossible de te répondre pour le moment !', null);
+            } else {
+                if (res.length === 0) {
+                    callback(null, [{
+                        text: 'A priori ' + search + ' n\'a jamais était joué :/'
+                    }]);
+                } else {
+                    res = res[0];
+
+                    let date = moment(res.createdAt).format('DD MMMM YYYY à HH:mm');
+                    res.text = search + ' a été joué pour la dernière fois sur cette putain de radio le ' + date;
+                    callback(null, [res]);
+                }
+            }
+        });
+    }
+
+    last10songs(callback) {
+        let query = {
+            find: {},
             sort: {
                 createdAt: 'desc'
             },
-            limit: 1
+            limit: 10
         };
-
-    that.db.find('histories', query, function(err, res) {
-        if (err) {
-            callback('#500 - Impossible de te répondre pour le moment !', null);
-        } else {
-            if (res.length === 0) {
-                callback(null, [{
-                    text: 'A priori ' + search + ' n\'a jamais était joué :/'
-                }]);
-            } else {
-                res = res[0];
-
-                let date = moment(res.createdAt).format('DD MMMM YYYY à HH:mm');
-                res.text = search + ' a été joué pour la dernière fois sur cette putain de radio le ' + date;
-                callback(null, [res]);
-            }
-        }
-    });
-}
-
-last10songs(callback) {
-    let query = {
-        find: {},
-        sort: {
-            createdAt: 'desc'
-        },
-        limit: 10
-    };
-    this.db.find('histories', query, function(err, histories) {
-        let results = [];
-        if (!err) {
-            histories.forEach(function(history) {
-                results.push({
-                    text: "- " + history.artist + ' - ' + history.songName,
-                    playedAt: history.createdAt,
-                    artist: history.artist,
-                    songName: history.songName
+        this.db.find('histories', query, function(err, histories) {
+            let results = [];
+            if (!err) {
+                histories.forEach(function(history) {
+                    results.push({
+                        text: "- " + history.artist + ' - ' + history.songName,
+                        playedAt: history.createdAt,
+                        artist: history.artist,
+                        songName: history.songName
+                    });
                 });
-            });
-        }
-        callback(err, results);
-    });
-}
+            }
+            callback(err, results);
+        });
+    }
 }
 
 module.exports = Statistics;
